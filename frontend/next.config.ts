2dde0e88a7aed5f03a1ebe7d865003a5147cb602
Amir <Amirmm.am2@outlook.com>
2025-12-24 23:32:13 -0800

    Reload reversion

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone", 
-  deploymentId: process.env.GIT_COMMIT_HASH || "latest",
 };
 
 export default nextConfig;
