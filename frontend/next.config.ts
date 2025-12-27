01fb26bb9570bc9b8569bcf39dac129633b0f5d4
Amir <Amirmm.am2@outlook.com>
2025-12-27 15:49:26 +0330

    Resource and Security

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone", 
+  // 1. Disable Powered By header (security)
+  poweredByHeader: false,
+  // 2. optimize images to use fewer resources on the server
+  images: {
+    unoptimized: true, // SAVES CPU: Lets the browser load images as-is (optional but recommended for low-CPU VPS)
+  },
+  // 3. Enable React strict mode for better error catching
+  reactStrictMode: true,
 };
 
 export default nextConfig;
